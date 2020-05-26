e08630a5f77de8b0f848883f2e0280873a3a1a0b
Vasyl-Pidgornyi <pidgornyi.vasyl@gmail.com>
2020-05-26 21:33:19 +0300

    upd

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  devtool: "source-map",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(jpg|jpeg|png|eot|ttf|svg|webp)$/,
+        use: [
+          {
+            loader: "url-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/",
+            },
+          },
+        ],
+      },
+      // {
+      //   test: /\.ttf$/,
+      //   use: [
+      //     {
+      //       loader: "ttf-loader",
+      //       options: {
+      //         name: "./font/[hash].[ext]",
+      //       },
+      //     },
+      //   ],
+      // },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
+};
